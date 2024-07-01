e161093e75fce2529e8b77fb350ebd65f9c45f26
NickCMoore <moorenicholasc@gmail.com>
2024-07-01 21:30:46 +0100

    CHORE: Create test homepage template

--- harmonia/harmonia/views.py
+++ harmonia/harmonia/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse("Welcome to the Harmonia homepage!")
+    # return HttpResponse("Welcome to the Harmonia homepage!")
+    return render(request, 'home.html')
