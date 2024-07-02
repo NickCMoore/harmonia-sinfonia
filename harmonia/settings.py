b1afbc79976583119cfbb8c4fc86d177f5cd77fd
NickCMoore <moorenicholasc@gmail.com>
2024-07-02 17:56:42 +0100

    FIX: Change settings to ensure Heroku deployment

--- harmonia/settings.py
+++ harmonia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,10 +85,7 @@ WSGI_APPLICATION = 'harmonia.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
