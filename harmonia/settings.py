9f0e2e6886a56a65d15efda9020d629d5fb0fb0e
NickCMoore <moorenicholasc@gmail.com>
2024-07-03 09:48:55 +0100

    CHORE: Add secret key

--- harmonia/settings.py
+++ harmonia/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4qu)&5vmpe&yi*wv0#r85wux=8!6kf=^$w1=bqdn9_$tk1gc-#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
