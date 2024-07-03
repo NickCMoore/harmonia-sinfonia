393574566f9c84f65823330bd2f13f6c6ff86e12
NickCMoore <moorenicholasc@gmail.com>
2024-07-03 09:23:41 +0100

    BUILD: Update database configuration

--- harmonia/settings.py
+++ harmonia/settings.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = 'harmonia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
