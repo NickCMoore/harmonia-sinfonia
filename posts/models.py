906765f32a6b24aea2ee938a86ea9d812bac81e1
NickCMoore <moorenicholasc@gmail.com>
2024-07-07 10:37:26 +0100

    FIX: Remove bug slug field from Post model

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from django.utils.text import slugify
-from django.utils.crypto import get_random_string
-
-# Create your models here.
 
 
 class Post(models.Model):
@@ -14,12 +10,6 @@ class Post(models.Model):
     image = CloudinaryField('image', blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='post_likes')
     is_flagged = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=255, blank=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.content[:50]) + '-' + get_random_string(6)
-        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.user.username} - {self.content[:20]}'
