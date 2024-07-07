3f2df32b5d556b3b17a0b7fed212f1cd89b1bacf
NickCMoore <moorenicholasc@gmail.com>
2024-07-07 10:07:32 +0100

    FIX: Update slug migration for existing records

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 # Create your models here.
 
@@ -12,7 +14,12 @@ class Post(models.Model):
     image = CloudinaryField('image', blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='post_likes')
     is_flagged = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.content[:50]) + '-' + get_random_string(6)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.user.username} - {self.content[:20]}'
--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 # Create your models here.
 
@@ -13,7 +15,12 @@ class Profile(models.Model):
     profile_pic = CloudinaryField('image')
     bg_pic = CloudinaryField('image', blank=True, null=True)
     followers = models.ManyToManyField(User, related_name='following')
-    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.content[:50]) + '-' + get_random_string(6)
+        super(Profile, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
