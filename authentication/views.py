d6633ca1a0ce2380b11a355768bbdd5cd55e4731
NickCMoore <moorenicholasc@gmail.com>
2024-07-08 14:32:42 +0100

    FIX: Ensure user is logged in on validation

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            form.save()
+            user = form.get_user()
+            login(request, user)
             return redirect("home:home")
     else:
         form = AuthenticationForm()
@@ -17,8 +19,9 @@ def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("home:home")
+            user = form.save()
+            login(request, user)
+            return redirect("posts:post_list")
     else:
         form = UserCreationForm()
     return render(request, "authentication/signup.html", {"form": form})
