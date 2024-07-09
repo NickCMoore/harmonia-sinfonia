3f2eb97ea4d207830ca1ef059293bb94f42a7557
NickCMoore <moorenicholasc@gmail.com>
2024-07-09 13:45:01 +0100

    FEAT: Add and link up post detail page

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'posts/post_list.html'
-    context_object_name = 'posts'
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+
+def post_detail_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
