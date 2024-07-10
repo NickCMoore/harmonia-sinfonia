c4651cb89ed6324b24589b186914c4cde311758e
NickCMoore <moorenicholasc@gmail.com>
2024-07-10 04:05:00 +0100

    FEAT: Add sign out functionality for users

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'authentication'
@@ -6,4 +7,6 @@ app_name = 'authentication'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
 ]
