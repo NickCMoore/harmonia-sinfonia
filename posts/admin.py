3d48c59ab666b6b82a4ab215dc334dc0007dff0d
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-13 08:31:50 +0100

    FEAT: Create Admin Actions for Unflagging

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,34 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+
+def unflag_posts(modeladmin, request, queryset):
+    queryset.update(is_flagged=False)
+    modeladmin.message_user(request, "Selected posts have been unflagged.")
+
+
+unflag_posts.short_description = "Unflag selected posts"
+
+
+def unflag_comments(modeladmin, request, queryset):
+    queryset.update(is_flagged=False)
+    modeladmin.message_user(request, "Selected comments have been unflagged.")
+
+
+unflag_comments.short_description = "Unflag selected comments"
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'is_flagged')
+    list_filter = ('is_flagged',)
+    actions = [unflag_posts]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'user', 'post', 'is_flagged')
+    list_filter = ('is_flagged',)
+    actions = [unflag_comments]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
