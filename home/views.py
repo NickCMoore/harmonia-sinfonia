e4007a1d508b2de2bef6aab7b699ab137ae36f97
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-15 18:34:51 +0100

    FIX: Return newsletter sign up back to home page

--- home/views.py
+++ home/views.py
@@ -1,22 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from profiles.models import Notification
+from newsletter.forms import NewsletterForm
+from django.contrib import messages
 
 
 def home_view(request):
     user = request.user
-    suspension_notification = None
-    other_notifications = None
-
-    if user.is_authenticated:
+    if request.user.is_authenticated:
         notifications = user.notifications.all()
         suspension_notification = notifications.filter(
             message__icontains='suspended').first()
         other_notifications = notifications.exclude(
             message__icontains='suspended')
+    else:
+        suspension_notification = None
+        other_notifications = None
+
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for subscribing!')
+            return redirect('home:home')
+    else:
+        form = NewsletterForm()
 
     context = {
         'suspension_notification': suspension_notification,
         'other_notifications': other_notifications,
+        'form': form
     }
     return render(request, 'home/home.html', context)
 
