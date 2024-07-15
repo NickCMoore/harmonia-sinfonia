18d4688eca3679c47246da70614507dd85e07697
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-15 12:21:50 +0100

    FIX: Remove flag posts id from DB and duplicates

--- posts/models.py
+++ posts/models.py
@@ -36,7 +36,6 @@ class Comment(models.Model):
 
 
 class Flag(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     reason = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
