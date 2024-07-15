614d2c5215b7f3cc21bd9f4d62a293772b5ab0c8
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-15 13:09:17 +0100

    FIX: Move suspension notification to notifications dropdown

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, Notification
 from django.contrib.auth.models import User
 
 
@@ -13,3 +13,14 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(post_save, sender=Profile)
+def send_suspension_notification(sender, instance, **kwargs):
+    if instance.is_suspended:
+        message = f'Your account has been suspended. Reason: {instance.suspension_reason}'
+        Notification.objects.get_or_create(
+            recipient=instance.user,
+            sender=None,
+            defaults={'message': message}
+        )
