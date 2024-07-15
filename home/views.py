b4aa59cf42e895965f6520f4c5002f958ea0e5aa
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-15 13:42:29 +0100

    FEAT: Add notifications context to views and update base template to display notifications dropdown correctly.

--- home/views.py
+++ home/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from profiles.models import Notification
 
 
+@login_required
 def home_view(request):
-    return render(request, 'home/home.html')
+    user = request.user
+    notifications = user.notifications.all()
+    suspension_notification = notifications.filter(
+        message__icontains='suspended').first()
+    other_notifications = notifications.exclude(message__icontains='suspended')
+
+    context = {
+        'suspension_notification': suspension_notification,
+        'other_notifications': other_notifications,
+    }
+    return render(request, 'home/home.html', context)
 
 
+@login_required
 def learn_more_view(request):
-    return render(request, 'home/learn_more.html')
+    user = request.user
+    notifications = user.notifications.all()
+    suspension_notification = notifications.filter(
+        message__icontains='suspended').first()
+    other_notifications = notifications.exclude(message__icontains='suspended')
+
+    context = {
+        'suspension_notification': suspension_notification,
+        'other_notifications': other_notifications,
+    }
+    return render(request, 'home/learn_more.html', context)
