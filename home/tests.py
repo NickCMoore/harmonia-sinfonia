f1932ba752fdf5b4d7f59a2acd6fd755536eb749
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-16 23:11:42 +0100

    TEST: Create tests for home app and home view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from profiles.models import Notification
+from newsletter.models import Subscriber 
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.notification1 = Notification.objects.create(user=self.user, message="Your account has been suspended.")
+        self.notification2 = Notification.objects.create(user=self.user, message="Welcome to the platform!")
+        self.subscriber = Subscriber.objects.create(email='subscriber@example.com')
+
+    def test_home_view_authenticated(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get(reverse('home:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.notification1.message)
+        self.assertContains(response, self.notification2.message)
+
+    def test_home_view_unauthenticated(self):
+        response = self.client.get(reverse('home:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.notification1.message)
+        self.assertNotContains(response, self.notification2.message)
