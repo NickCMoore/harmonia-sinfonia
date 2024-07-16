388b6b8578ce6b209b1488e2296198fa49790eaf
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-16 23:29:54 +0100

    TEST: Remove newsletter tests and do imports for posts app test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post, Comment, Flag
+from .forms import PostForm, CommentForm, FlagForm
 
-# Create your tests here.
+class PostTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.post = Post.objects.create(title='Test Post', content='Test content', user=self.user)
+        self.comment = Comment.objects.create(content='Test comment', post=self.post, user=self.user)
+    
