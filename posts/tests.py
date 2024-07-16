80ed3840438b83aea898611986ec5034bcd21086
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-16 23:30:40 +0100

    TEST: Create post list and detail view tests

--- posts/tests.py
+++ posts/tests.py
@@ -9,4 +9,27 @@ class PostTests(TestCase):
         self.user = User.objects.create_user(username='testuser', password='12345')
         self.post = Post.objects.create(title='Test Post', content='Test content', user=self.user)
         self.comment = Comment.objects.create(content='Test comment', post=self.post, user=self.user)
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('posts:post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts/post_list.html')
+        self.assertContains(response, self.post.title)
+
+    def test_post_detail_view(self):
+        response = self.client.get(reverse('posts:post_detail', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts/post_detail.html')
+        self.assertContains(response, self.post.title)
+
+    def test_create_post_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('posts:create_post'), {
+            'title': 'New Post',
+            'content': 'New content',
+            'user': self.user.id
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.count(), 2)
+
     
