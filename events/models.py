b2c7fecf495ef553c34f80f1d5106ffae05d889b
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-17 00:55:21 +0100

    REFACTOR: Further docstrings for events app

--- events/models.py
+++ events/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Event(models.Model):
+    """Model representing an event."""
     title = models.CharField(max_length=200)
     date = models.DateTimeField()
     content = models.TextField()
-    liked_by = models.ManyToManyField(
-        User, related_name='liked_events', blank=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_events', blank=True)
     time = models.TimeField()
-    image = models.ImageField(
-        upload_to='events/images/', blank=True, null=True)
+    image = models.ImageField(upload_to='events/images/', blank=True, null=True)
 
     def __str__(self):
+        """Return the event title as its string representation."""
         return self.title
 
     def total_likes(self):
+        """Return the total number of likes for the event."""
         return self.liked_by.count()
--- events/views.py
+++ events/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 from .models import Event
 
-
 def event_detail_view(request, pk):
+    """Display the details of a specific event."""
     event = get_object_or_404(Event, pk=pk)
     return render(request, 'events/event_detail.html', {'event': event})
 
-
 def event_list_view(request):
+    """Display a list of all events."""
     events = Event.objects.all()
     return render(request, 'events/event_list.html', {'events': events})
 
-
 @login_required
 def like_event(request, event_id):
-    event = get_object_or_404(Event, id=event_id)
-    if request.user in event.liked_by.all():
-        event.liked_by.remove(request.user)
-        messages.success(request, f'You unliked {event.title}.')
-    else:
-        event.liked_by.add(request.user)
-        messages.success(request, f'You liked {event.title}.')
+    """Allow a logged-in user to like or unlike an event."""
+    try:
+        event = get_object_or_404(Event, id=event_id)
+        if request.user in event.liked_by.all():
+            event.liked_by.remove(request.user)
+            messages.success(request, f'You unliked {event.title}.')
+        else:
+            event.liked_by.add(request.user)
+            messages.success(request, f'You liked {event.title}.')
+    except Event.DoesNotExist:
+        messages.error(request, "The event you are trying to like does not exist.")
+    except ValidationError as e:
+        messages.error(request, f"Error during liking the event: {e}")
+    except Exception as e:
+        messages.error(request, f"An unexpected error occurred: {e}")
+
     return redirect('events:event_detail', pk=event.id)
