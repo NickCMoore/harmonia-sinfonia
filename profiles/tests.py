a2b48b6cb45d7cf84b72f562ad4fe463dae0db10
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-16 23:35:18 +0100

    TEST: Add improts and setUp for Profiles app test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile, Notification
+from posts.models import Post
+from django.utils import timezone
 
-# Create your tests here.
+class ProfileTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile.objects.create(user=self.user, display_name='Test User')
+        self.notification = Notification.objects.create(recipient=self.user, message='Test notification')
+        self.user2 = User.objects.create_user(username='testuser2', password='12345')
+        self.profile2 = Profile.objects.create(user=self.user2, display_name='Test User 2')
+        self.post = Post.objects.create(content='Test post content', user=self.user)
+
