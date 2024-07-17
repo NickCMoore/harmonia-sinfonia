11de3059b1018a7fc7d83e69d839bd2528822cc2
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-17 01:02:17 +0100

    REFACTOR: Docstrings for the profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -3,43 +3,36 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.utils.crypto import get_random_string
 
-# Profile model
-
-
 class Profile(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='profile')
+    """Model representing a user profile."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     display_name = models.CharField(max_length=255)
     bio = models.TextField(blank=True, null=True)
     profile_pic = models.ImageField(upload_to='profiles/images/')
-    bg_pic = models.ImageField(
-        upload_to='profiles/bg_images/', blank=True, null=True)
-    followers = models.ManyToManyField(
-        User, related_name='following', blank=True)
+    bg_pic = models.ImageField(upload_to='profiles/bg_images/', blank=True, null=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
     slug = models.SlugField(max_length=255, blank=True)
     is_suspended = models.BooleanField(default=False)
     suspension_reason = models.TextField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
+        """Save the profile, generating a slug if not present."""
         if not self.slug:
-            self.slug = slugify(self.display_name)[
-                :50] + '-' + get_random_string(6)
+            self.slug = slugify(self.display_name)[:50] + '-' + get_random_string(6)
         super(Profile, self).save(*args, **kwargs)
 
     def __str__(self):
+        """Return a string representation of the profile."""
         return self.user.username
 
-# Notification model
-
-
 class Notification(models.Model):
-    recipient = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='notifications')
-    sender = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='sent_notifications', null=True, blank=True)
+    """Model representing a notification."""
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_notifications', null=True, blank=True)
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
 
     def __str__(self):
+        """Return a string representation of the notification."""
         return f'Notification from {self.sender} to {self.recipient}'
--- profiles/views.py
+++ profiles/views.py
@@ -6,25 +6,25 @@ from .models import Profile, Notification
 from posts.models import Post
 from .forms import UserProfileForm, SearchForm
 
-
 def is_admin(user):
+    """Check if the user is an admin."""
     return user.is_staff
-
 
 @login_required
 def profile_list_view(request):
+    """Display a list of user profiles."""
     profiles = Profile.objects.all()
     return render(request, 'profiles/profile_list.html', {'profiles': profiles})
 
-
 @login_required
 def profile_detail_view(request, username):
+    """Display the details of a specific user profile."""
     profile = get_object_or_404(Profile, user__username=username)
     return render(request, 'profiles/profile_detail.html', {'user_profile': profile})
 
-
 @login_required
 def follow_unfollow(request, username):
+    """Toggle following and unfollowing a user."""
     target_user = get_object_or_404(User, username=username)
     profile = target_user.profile
 
@@ -43,57 +43,54 @@ def follow_unfollow(request, username):
     messages.success(request, message)
     return redirect('profiles:profile_detail', username=username)
 
-
 @login_required
 def following_list(request):
+    """Display the list of users the current user is following."""
     following = request.user.profile.followers.all()
     return render(request, 'profiles/following_list.html', {'following': following})
 
-
 @login_required
 def notifications_list(request):
+    """Display the list of notifications for the current user."""
     notifications = request.user.notifications.all()
     return render(request, 'profiles/notifications_list.html', {'notifications': notifications})
 
-
 @login_required
 def mark_notification_as_read(request, notification_id):
-    notification = get_object_or_404(
-        Notification, id=notification_id, recipient=request.user)
+    """Mark a notification as read."""
+    notification = get_object_or_404(Notification, id=notification_id, recipient=request.user)
     notification.read = True
     notification.save()
     return redirect('profiles:notifications_list')
 
-
 @login_required
 def delete_notification(request, notification_id):
-    notification = get_object_or_404(
-        Notification, id=notification_id, recipient=request.user)
+    """Delete a notification."""
+    notification = get_object_or_404(Notification, id=notification_id, recipient=request.user)
     notification.delete()
     return redirect('profiles:notifications_list')
 
-
 @login_required
 def edit_profile(request, username):
+    """Edit a user profile."""
     user_profile = get_object_or_404(Profile, user__username=username)
 
     if request.user == user_profile.user or request.user.is_staff:
         if request.method == 'POST':
-            form = UserProfileForm(
-                request.POST, request.FILES, instance=user_profile)
+            form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
             if form.is_valid():
                 form.save()
+                messages.success(request, 'Profile updated successfully.')
                 return redirect('profiles:profile_detail', username=username)
         else:
             form = UserProfileForm(instance=user_profile)
         return render(request, 'profiles/edit_profile.html', {'form': form})
     else:
-        messages.error(
-            request, "You do not have permission to edit this profile.")
+        messages.error(request, "You do not have permission to edit this profile.")
         return redirect('profiles:profile_detail', username=username)
 
-
 def search_view(request):
+    """Search for users or posts."""
     form = SearchForm(request.GET or None)
     query = None
     filter_by = None
@@ -105,8 +102,7 @@ def search_view(request):
         filter_by = form.cleaned_data.get('filter_by')
 
         if filter_by == 'users' or not filter_by:
-            user_results = Profile.objects.filter(
-                user__username__icontains=query)
+            user_results = Profile.objects.filter(user__username__icontains=query)
         if filter_by == 'posts' or not filter_by:
             post_results = Post.objects.filter(content__icontains=query)
 
