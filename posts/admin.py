4c8cb1c58b7f7466cc9f32ef9e66c6d4207e0418
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-18 12:22:03 +0100

    REFACTOR: Add further docstrings to admin and tests files in posts app

--- posts/admin.py
+++ posts/admin.py
@@ -1,34 +1,31 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-
 def unflag_posts(modeladmin, request, queryset):
+    """Unflag selected posts."""
     queryset.update(is_flagged=False)
     modeladmin.message_user(request, "Selected posts have been unflagged.")
 
-
 unflag_posts.short_description = "Unflag selected posts"
 
-
 def unflag_comments(modeladmin, request, queryset):
+    """Unflag selected comments."""
     queryset.update(is_flagged=False)
     modeladmin.message_user(request, "Selected comments have been unflagged.")
 
-
 unflag_comments.short_description = "Unflag selected comments"
 
-
 class PostAdmin(admin.ModelAdmin):
+    """Admin configuration for the Post model."""
     list_display = ('title', 'user', 'is_flagged')
     list_filter = ('is_flagged', 'user', 'title')
     actions = [unflag_posts]
 
-
 class CommentAdmin(admin.ModelAdmin):
+    """Admin configuration for the Comment model."""
     list_display = ('content', 'user', 'post', 'is_flagged')
     list_filter = ('is_flagged', 'user', 'post')
     actions = [unflag_comments]
-
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
--- posts/tests.py
+++ posts/tests.py
@@ -6,23 +6,27 @@ from .forms import PostForm, CommentForm, FlagForm
 
 class PostTests(TestCase):
     def setUp(self):
+        """Set up the test environment with a user, post, and comment."""
         self.user = User.objects.create_user(username='testuser', password='12345')
         self.post = Post.objects.create(title='Test Post', content='Test content', user=self.user)
         self.comment = Comment.objects.create(content='Test comment', post=self.post, user=self.user)
 
     def test_post_list_view(self):
+        """Test the post list view loads correctly."""
         response = self.client.get(reverse('posts:post_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'posts/post_list.html')
         self.assertContains(response, self.post.title)
 
     def test_post_detail_view(self):
+        """Test the post detail view loads correctly."""
         response = self.client.get(reverse('posts:post_detail', args=[self.post.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'posts/post_detail.html')
         self.assertContains(response, self.post.title)
 
     def test_create_post_view(self):
+        """Test creating a new post."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:create_post'), {
             'title': 'New Post',
@@ -33,30 +37,35 @@ class PostTests(TestCase):
         self.assertEqual(Post.objects.count(), 2)
 
     def test_delete_post_view(self):
+        """Test deleting a post."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:delete_post', args=[self.post.pk]))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Post.objects.count(), 0)
 
     def test_toggle_like_view(self):
+        """Test toggling a like on a post."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:toggle_like', args=[self.post.pk]))
         self.assertEqual(response.status_code, 302)
         self.assertTrue(self.user in self.post.likes.all())
 
     def test_delete_comment_view(self):
+        """Test deleting a comment."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:delete_comment', args=[self.comment.pk]))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Comment.objects.count(), 0)
 
     def test_toggle_upvote_comment(self):
+        """Test toggling an upvote on a comment."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:toggle_upvote_comment', args=[self.comment.pk]))
         self.assertEqual(response.status_code, 302)
         self.assertTrue(self.user in self.comment.upvotes.all())
 
     def test_edit_post_view(self):
+        """Test editing a post."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:edit_post', args=[self.post.pk]), {
             'title': 'Updated Post',
@@ -67,6 +76,7 @@ class PostTests(TestCase):
         self.assertEqual(self.post.title, 'Updated Post')
 
     def test_flag_post(self):
+        """Test flagging a post."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:flag_post', args=[self.post.pk]), {
             'reason': 'Inappropriate content'
@@ -76,6 +86,7 @@ class PostTests(TestCase):
         self.assertTrue(self.post.is_flagged)
 
     def test_flag_comment(self):
+        """Test flagging a comment."""
         self.client.login(username='testuser', password='12345')
         response = self.client.post(reverse('posts:flag_comment', args=[self.comment.pk]), {
             'reason': 'Inappropriate content'
@@ -83,5 +94,3 @@ class PostTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.comment.refresh_from_db()
         self.assertTrue(self.comment.is_flagged)
-
-    
