d0815ee0c655a0e385c0c0dd7ec72b87804e9be9
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-18 12:15:59 +0100

    REFACTOR: Add further docstrings for events app

--- events/admin.py
+++ events/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
+    """Admin configuration for the Event model."""
     list_display = ('title', 'date', 'time', 'image', 'total_likes')
 
     def total_likes(self, obj):
+        """Return the total number of likes for the event."""
         return obj.total_likes()
 
 admin.site.register(Event, EventAdmin)
--- events/tests.py
+++ events/tests.py
@@ -6,21 +6,25 @@ from .models import Event
 class EventTests(TestCase):
 
     def setUp(self):
+        """Set up test environment with a user and an event."""
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='password')
         self.event = Event.objects.create(title='Test Event', date='2024-12-31', time='12:00:00')
 
     def test_event_detail_view(self):
+        """Test the event detail view loads correctly."""
         response = self.client.get(reverse('events:event_detail', args=[self.event.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.event.title)
 
     def test_event_list_view(self):
+        """Test the event list view loads correctly."""
         response = self.client.get(reverse('events:event_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.event.title)
 
     def test_like_event(self):
+        """Test liking and unliking an event."""
         self.client.login(username='testuser', password='password')
         
         response = self.client.post(reverse('events:like_event', args=[self.event.id]))
@@ -32,7 +36,7 @@ class EventTests(TestCase):
         self.assertFalse(self.user in self.event.liked_by.all())
 
     def test_like_event_requires_login(self):
+        """Test liking an event requires user to be logged in."""
         response = self.client.post(reverse('events:like_event', args=[self.event.id]))
         self.assertNotEqual(response.status_code, 200)
         self.assertFalse(self.user in self.event.liked_by.all())
-
