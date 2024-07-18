5bbfd5e6a90ec546f0af1c8f12df4fcc05280df3
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-07-18 12:17:31 +0100

    REFACTOR: Add more dosctrings for views.py in harmonia app

--- harmonia/views.py
+++ harmonia/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-
 def home(request):
+    """Render the home page."""
     return render(request, 'home.html')
 
-
 def learn_more(request):
+    """Render the learn more page."""
     return render(request, 'learn_more.html')
